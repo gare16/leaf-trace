c07c164a298381df191bb167e40b9ba2e68a1b48
gare16 <tegar.10119911@mahasiswa.unikom.ac.id>
2025-08-12 08:26:51 +0700

    fix: error history not found

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { getDate } from "@/lib/date";
-import DashboardClient from "@/components/dashboard-client";
 import { MQTTProvider } from "../../context/mqtt-context";
 import { getData } from "@/services/brake-service";
+import DashboardClient from "@/components/dashboard-client";
 
 export default async function DashboardPage() {
   const { date } = getDate();
   const dataSpesific = await getData(date);
+  console.log(date);
   return (
     <MQTTProvider>
       <DashboardClient messages={dataSpesific} />
